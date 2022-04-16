88fb6e31fcee5fddd198940c799f02afd805291d
AhshanHabib26 <habib.net27@gmail.com>
2022-04-17 04:14:25 +0600

    Login Component Auth Setup

--- src/Component/Pages/Header/Header.js
+++ src/Component/Pages/Header/Header.js
@@ -1,9 +1,19 @@
+import { signOut } from "firebase/auth";
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../../app_firebase_init";
 import './Header.css'
 
+
 const Header = () => {
+  
+  const [user] = useAuthState(auth);
+  const logout = () => {
+    signOut(auth);
+  };
+
   return (
     <div>
       <Navbar bg="light" expand="lg">
@@ -17,8 +27,14 @@ const Header = () => {
               navbarScroll
             >
               <Nav.Link as={Link} to='/blog'>Blog</Nav.Link>
-              <Nav.Link as={Link} to='/login'>Login</Nav.Link>
-              <Nav.Link as={Link} to='/signup'>Register</Nav.Link>
+            </Nav>
+            <Nav>
+              {user ? (
+                <Nav.Link onClick={logout} >Logout</Nav.Link>
+              ) : (
+                <Nav.Link as={Link} to='/login'>Login</Nav.Link>
+          
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
