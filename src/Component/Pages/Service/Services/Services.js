06486a45b27a2cb8ab32403404cf488ea9f33bc2
AhshanHabib26 <habib.net27@gmail.com>
2022-04-17 22:18:50 +0600

    Support Componnet Responsive

--- src/Component/Pages/Service/Services/Services.js
+++ src/Component/Pages/Service/Services/Services.js
@@ -10,7 +10,7 @@ const Services = () => {
   return (
     <div className="services_sec container">
       <h1 className="my-5 services_title"> <span>Top Rated</span>  Learning Tutorials</h1>
-      <Row xs={1} md={3} lg={3}>
+      <Row xs={1} md={2} lg={3}>
         {services.map((service) => (
           <Service service={service} key={service.Id} />
         ))}
